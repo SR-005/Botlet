d1b3f4d6fb1431e745e0a3ad90bd2212b02b886d
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-07-06 12:19:52 +0530

    creating basic bot response

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,request,render_template
 
 app=Flask(__name__)
-@app.route("/")
+@app.route("/",methods=["GET","POST"])
 def index():
     return render_template("index.html")
 
