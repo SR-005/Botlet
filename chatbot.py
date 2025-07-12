67c2098581b5bbd9a9da0be1f33d0ce97ad45f59
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-07-12 22:33:46 +0530

    small issue fixed

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,15 @@
 from dotenv import load_dotenv
 import os
+import openai
 
 #for loading API Key into file
 load_dotenv()
 apikey = os.getenv("API_KEY")
 
-def main():
+def main(userinput):
+    try:
+        response=openai.ChatCompletion.create()
+    except:
+        pass
     botreply=""
     return botreply
