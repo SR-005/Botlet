0570224c08430a81be4737de2d5641ba214e2d9b
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-07-12 21:58:18 +0530

    connecting bot

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from flask import Flask,request,render_template,jsonify
 app=Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 def index():
-    userinput=request.form.get("userinput")
-    #userinput=userinput.lower()
-    return render_template("index.html")
+    botreply=""
+    if request.method == "POST":
+        userinput=request.form.get("userinput")
+        userinput=userinput.lower()
+        return render_template("index.html")
 
 if __name__=="__main__":
     app.run(debug=True)
