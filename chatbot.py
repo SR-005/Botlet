fb0100a276f05a0f6260c0ec655547b4604e3df8
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-07-13 13:16:36 +0530

    gemini integratio completed

--- chatbot.py
+++ chatbot.py
@@ -1,27 +1,23 @@
-from dotenv import load_dotenv
 import os
-import openai
+import google.generativeai as genai
+import json
 
 #for loading API Key into file
-load_dotenv()
-apikey = os.getenv("API_KEY")
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "directed-craft-465717-c8-d655a7741d5c.json"
+
+# Configure the model
+genai.configure()
 
 model = genai.GenerativeModel("gemini-pro")
 def main(userinput):
     try:
-        #getting respose from api
-        response=openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-             messages=[
-                    {"role": "system", "content": "You are a helpful and friendly chatbot."},
-                    {"role": "user", "content": userinput}
-                ]
-        )
-        #selecting respose to user from api return
-        botreply = response["choices"][0]["message"]["content"].strip()
+        response=model.generate_content(user_input)
+        botreply=response.text    
+
     except Exception as e:
         botreply = f"Error: {str(e)}"
+
     print(botreply)
     return botreply
 
-main(userinput="what is the date today?")
+main("what is the date today?")
