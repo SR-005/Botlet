47f664cfbee45863b2588634b3d9326099397011
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-07-17 00:12:05 +0530

    History added

--- app.py
+++ app.py
@@ -6,16 +6,17 @@ app=Flask(__name__)
 def index():
     botreply=None
     userinput=None
-    
+    history=None
+
     if request.method == "POST":
         #getting message from the user
         userinput=request.form.get("userinput")
         #userinput="tell me a joke"
         print(userinput)
 
-        botreply=botautomation(userinput)
+        botreply,history=botautomation(userinput)
         print(botreply)
-    return render_template("index.html",botreply=botreply,userinput=userinput)
+    return render_template("index.html",botreply=botreply,userinput=userinput,history=history)
     
 if __name__=="__main__":
     app.run(debug=True)
--- chatbot.py
+++ chatbot.py
@@ -2,21 +2,22 @@ import os
 from dotenv import load_dotenv
 import google.generativeai as genai
 
+print("-----entered mainfun------")
+#Load and Read API Ket from .env file
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+
+#configure Gemini SDK
+genai.configure(api_key=api_key)
+#setting up gemini model
+model = genai.GenerativeModel("gemini-1.5-flash") 
+chat = model.start_chat(history=[])
+
 #API Fetch Function
 def main(userinput):
-    print("-----entered mainfun------")
-    #Load and Read API Ket from .env file
-    load_dotenv()
-    api_key = os.getenv("GEMINI_API_KEY")
-
-    #configure Gemini SDK
-    genai.configure(api_key=api_key)
-    #setting up gemini model
-    model = genai.GenerativeModel("gemini-1.5-flash") 
-    chat = model.start_chat(history=[])
     try:
         response = chat.send_message(userinput)
-        return response.text
+        return response.text, chat.history
     except Exception as e:
         return f"Error: {e}"
 
